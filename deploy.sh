2f9ef011cb2af15697829bd4cc19553ba9a98ecb
唐磊 <tanglei@ricelink.cn>
2019-04-07 10:10:42 +0800

    添加tomact自动部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,42 @@
 #!/usr/bin/env bash
-#编译+部署码果
+#编译+部署order站点
 
 #需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
 # export PROJ_PATH=这个jenkins任务在部署机器上的路径
 
+# 输入你的环境上tomcat的全路径
+# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
 
 ### base 函数
-killBoot()
+killTomcat()
 {
-  pid=`ps -ef|grep code|grep java|awk '{print $2}'`
-  echo "codefruit pid :$pid"
-  if [ "$pid" = ""]
-  then
-    echo "no tomcat pid alive"
-  else
-    kill -9 $pid
-  fi
+    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+    echo "tomcat Id list :$pid"
+    if [ "$pid" = "" ]
+    then
+      echo "no tomcat pid alive"
+    else
+      kill -9 $pid
+    fi
 }
+cd $PROJ_PATH/order
+mvn clean install
 
-cd $PROJ_PATH/codefruit
-mvn -Dmaven.test.skip -U clean package
+# 停tomcat
+killTomcat
 
-#停止Boot
-killBoot
+# 删除原有工程
+rm -rf $TOMCAT_APP_PATH/webapps/ROOT
+rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
+rm -f $TOMCAT_APP_PATH/webapps/order.war
 
-#启动项目
-nohup java -jar target/code_fruit_web-0.0.1-SNAPSHOT.jar &
+# 复制新的工程
+cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+
+cd $TOMCAT_APP_PATH/webapps/
+mv order.war ROOT.war
+
+# 启动Tomcat
+cd $TOMCAT_APP_PATH/
+sh bin/startup.sh
